2dcb49c8c07a9af11185648de55d905d4a372c4c
Dihfahsih1 <dihfahsihm@gmail.com>
2022-06-10 00:17:22 +0300

    fix bug

--- apps/students/admin.py
+++ apps/students/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import *
+from apps.corecode.models import *
 
 admin.site.register(Student)
 admin.site.register(StudentBulkUpload)
+admin.site.register(AcademicSession)
+admin.site.register(AcademicTerm)
 
 # Register your models here.
